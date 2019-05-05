0efda5a29d595173c8e6ac1087dc07ac7bf2b5e3
yuukiik <yu20001587ik26@gmail.com>
2019-05-05 19:04:41 +0000

    rails kadai

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,57 @@
+class TasksController < ApplicationController
+  
+  def index 
+    @task = Task.all
+  end
+  
+  def show
+    @task = Task.find(params[:id])
+  end
+  
+  def new
+    @task = Task.new
+  end
+  
+  def edit
+    @task = Task.find(params[:id])
+  end
+  
+  def create
+    @task = Task.new(tasks_params)
+    
+    if @task.save
+      flash[:success] = '正常に投稿されました'
+      redirect_to @task
+    else
+      flash.now[:alert] = '投稿できませんでした'
+      render :new
+    end
+  end
+  
+  def update
+    @task = Task.find(params[:id])
+    
+    if @task.update(tasks_params)
+      flash[:success] = '正常に更新されました'
+      redirect_to @task
+    else
+      flash.now[:alert] = '更新できませんでした'
+      render :edit
+    end
+  end
+  
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    
+    flash[:success] = '正常に削除されました'
+    redirect_to tasks_url
+  end
+  
+end
+
+private
+
+def tasks_params
+  params.require(:task).permit(:content)
+end
