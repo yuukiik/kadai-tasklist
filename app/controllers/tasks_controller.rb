01e8997d83ab7bbe6a94268fb96bd7d921f5d912
yuukiik <yu20001587ik26@gmail.com>
2019-05-16 10:47:20 +0000

    kadai

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,6 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
+  before_action :correct_user, only: [:destroy]
     
   def index 
    # @task = Task.all
@@ -59,6 +60,12 @@ class TasksController < ApplicationController
     params.require(:task).permit(:content, :status)
   end
   
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
+  end
 
   
 end
